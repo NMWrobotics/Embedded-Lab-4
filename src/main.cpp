91b7922bd245e170b48d672d5251844a035264ba
Nimeth Menuka <nimethm5@gmail.com>
2024-03-13 22:52:50 +0530

    add function delay_ms()

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,66 @@
 #include <Arduino.h>
 
+unsigned long milliSeconds;
+unsigned long us;
 
 void setup() {
-  //DDRB = 0;
-  //DDRB |=  (1<<5);
+  Serial.begin(9600);
+  DDRB = 0;
+  DDRB |=  (1<<5);
   //PORTB |= (1<<5);
-  //setup timer 1 for toggling led 13 every 1 second
-  TCCR1A =0;
-  TCCR1B =0;
-  TCNT1 =0;
 
-  //chanel A compare value
-  OCR1A = 15625;
+  cli();
+  //setup timer 1 for toggling led 13 every 1 second
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  TCNT1 = 0;
+
+  //set Timer 1 for count miliseconds
+  //calculating OCR1A
+  //1ms 1KHz
+  //OCR1A = (16,000,000/(pre scalar*1000)) - 1 
+  //OCR1A = (16,000,000/(1*1000)) - 1 = 15999
+  //OCR1A = (16,000,000/(8*1000)) - 1 = 1999
+  //OCR1A = (16,000,000/(64*1000)) - 1 = 249
+  //OCR1A = (16,000,000/(256*1000)) - 1 = 61.5
+  //OCR1A = (16,000,000/(1024*1000)) - 1 = 14.625
+  //lets choose 64 as the pre scalar then OCR1A = 249
+  OCR1A = 249;
   TCCR1B |= (1 << WGM12); //ctc mode
-  TCCR1B |= (1 << CS12) | (1 << CS10); // set presca;ar
+  TCCR1B |= (1 << CS10) | (1 << CS11); // set prescalar
   TIMSK1 |= ( 1 << OCIE1A); //match A interrupt enable
   sei(); //enable glpbal interruptd
 
-  DDRB |= (1 << DDB5);  //set pin 13 a s output
+  
+  
+  //DDRB |= (1 << DDB5);  //set pin 13 a s output
 
 }
 
 ISR(TIMER1_COMPA_vect){
-  PORTB ^= (1 << PORTB5);
+  //PORTB ^= (1 << PORTB5);
+  milliSeconds++;
+}
+void delay_ms(unsigned long ms){
+  unsigned long current_ms = milliSeconds;
+  while(true){
+    if(milliSeconds - current_ms >= ms){
+      break;
+    }
+    Serial.println("yes");
+  }
 }
 
 void loop() {
   //delay(500);
   //PORTB &= ~(1<<5);
   //delay(500);
-  //PORTB |= (1<<5);
-
+  PORTB |= (1<<5);
+  delay_ms(3000);
+  //delay(3000);
+  PORTB &= ~(1<<5);
+  delay_ms(1000);
+  //delay(1000);
    
 }
